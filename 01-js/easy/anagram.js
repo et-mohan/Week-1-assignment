b2aca2c68d166567b79458ae6b86a799a44d0ebe
et_mohan <etmohan7@gmail.com>
2023-11-20 14:31:42 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,31 @@
 */
 
 function isAnagram(str1, str2) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  const cleanStr1 = str1.replace(/[^\w]/g, '').toLowerCase();
+  const cleanStr2 = str2.replace(/[^\w]/g, '').toLowerCase();
 
+  // Check if the lengths are the same
+  if (cleanStr1.length !== cleanStr2.length) {
+      return false;
+  }
+
+  // Sort the characters and compare the sorted strings
+  const sortedStr1 = cleanStr1.split('').sort().join('');
+  const sortedStr2 = cleanStr2.split('').sort().join('');
+
+  return sortedStr1 === sortedStr2;
 }
+
+// Example usage:
+const string1 = "listen";
+const string2 = "silent";
+
+if (isAnagram(string1, string2)) {
+  console.log(`${string1} and ${string2} are anagrams.`);
+} else {
+  console.log(`${string1} and ${string2} are not anagrams.`);
+}
+
 
 module.exports = isAnagram;
