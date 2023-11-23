34e48c7a81792c83dfb6f40ecf2498013b470b92
et_mohan <etmohan7@gmail.com>
2023-11-23 15:02:24 +0530

    Update palindrome.js

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -7,7 +7,21 @@
 */
 
 function isPalindrome(str) {
+  var left=0
+  var right=str.length-1
+ 
+  while(left < right) {
+    if(str[left]!==str[right]){
+        return false
+    }
+    left++;
+    right--;
+  }
+  
   return true;
 }
+str=isPalindrome("1991")
+console.log(str)
+
 
 module.exports = isPalindrome;
